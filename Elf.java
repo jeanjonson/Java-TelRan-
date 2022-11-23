4c02fbdcc41c32c3c73c782cee142e4aae5951c4
Zans <117941042+jeanjonson@users.noreply.github.com>
2022-11-23 21:51:30 +0200

    Add files via upload

---
+++ Elf.java
@@ -0,0 +1,17 @@
+package Game.hero;
+
+public class Elf {
+    static int ElfPower = 65;
+    static int ElfCharisma = 35;
+    static int ElfMagic = 50;
+    public static void main(String[] args) {
+
+
+        int id = 123;
+        int balance = 250;
+        boolean fast = true;
+        int weapon = 100;
+        int height = 113;
+        boolean premium = true
+    }
+}
